4543ae496bcea232824ad43fcdc24f03e1664a27
Adrian Dusa <dusa.adrian@gmail.com>
2021-10-26 09:41:59 +0300

    update la win32 si mesaje eroare

--- R/hideFiles.R
+++ R/hideFiles.R
@@ -3,10 +3,27 @@ hideFiles <- function(gitdir = NULL, project = NULL) {
     if (is.null(project)) {
         stop("Numele proiectului lipseste.")
     }
-    
+
+    if (is.null(gitdir)) {
+        gitdir <- "C:/Users/adrian/Documents/GitHub"
+    }
+
+    if (!file.exists(file.path(gitdir, project, "build"))) {
+        stop("Directorul de build nu exista.")
+    }
+
+    if (!file.exists(file.path(gitdir, project, "build", "output"))) {
+        stop("Nu am gasit niciun build, directorul output nu exista.")
+    }
+
+    if (!file.exists(file.path(gitdir, project, "build", "output", "win-unpacked"))) {
+        stop("Directorul win-unpacked nu exista.")
+    }
+
+    win32 <- file.exists(file.path(gitdir, project, "build", "output", "win-ia32-unpacked"))
+
     dir <- file.path(
-        ifelse(is.null(gitdir), "C:/Users/adrian/Documents/GitHub", gitdir),
-        project, "build", "output", "win-unpacked"
+        gitdir, project, "build", "output", "win-unpacked"
     )
 
     files <- setdiff(
@@ -20,4 +37,22 @@ hideFiles <- function(gitdir = NULL, project = NULL) {
 
     system(paste("attrib +h", file.path(dir, "resources", "assets")))
     system(paste("attrib +h", file.path(dir, "resources", "app.asar")))
+
+    if (win32) {
+        dir <- file.path(
+            gitdir, project, "build", "output", "win-ia32-unpacked"
+        )
+
+        files <- setdiff(
+            list.files(dir),
+            c("WorldBankTool.exe", "resources", "LICENSES.chromium.html", "LICENSE.electron.txt")
+        )
+        
+        for (i in seq(length(files))) {
+            system(paste("attrib +h", file.path(dir, files[i])))
+        }
+
+        system(paste("attrib +h", file.path(dir, "resources", "assets")))
+        system(paste("attrib +h", file.path(dir, "resources", "app.asar")))
+    }
 }
