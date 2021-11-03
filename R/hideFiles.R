c8f1d577d85e1d749aa415be8d99bf31eb9c594c
Adrian Dusa <dusa.adrian@gmail.com>
2021-11-03 09:59:57 +0200

    ascunde si directorul page din resources

--- R/hideFiles.R
+++ R/hideFiles.R
@@ -41,6 +41,9 @@ hideFiles <- function(gitdir = NULL, project = NULL) {
 
     system(paste("attrib +h", file.path(dir, "resources", "assets")))
     system(paste("attrib +h", file.path(dir, "resources", "app.asar")))
+    if (file.exists(file.path(dir, "resources", "page"))) {
+        system(paste("attrib +h", file.path(dir, "resources", "page")))
+    }
 
     if (win32) {
         dir <- file.path(
@@ -58,5 +61,8 @@ hideFiles <- function(gitdir = NULL, project = NULL) {
 
         system(paste("attrib +h", file.path(dir, "resources", "assets")))
         system(paste("attrib +h", file.path(dir, "resources", "app.asar")))
+        if (file.exists(file.path(dir, "resources", "page"))) {
+            system(paste("attrib +h", file.path(dir, "resources", "page")))
+        }
     }
 }
